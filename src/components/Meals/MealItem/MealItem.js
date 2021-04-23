78150cf60d64c8ba2162c074a6598eadb3458263
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-23 18:26:10 +0300

    MealItem component updated

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = ({ meal }) => {
   const { name, description, price } = meal;
 
   return (
-    <li>
-      <div className={classes.MealItem}>
-        <span className={classes.MealName}>{name}</span>
-        <span className={classes.Desc}>{description}</span>
-        <span className={classes.MealPrice}>${price}</span>
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>${price.toFixed(2)}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
