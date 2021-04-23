0bf7c1652eb3c0f9bb08624e748c60029c6795ac
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-23 17:59:45 +0300

    Meals added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header/Header';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
