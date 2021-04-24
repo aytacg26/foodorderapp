92f0716c4113440dba16418c733be0c455d217bc
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-24 23:14:34 +0300

    Cart Context used to add data to the button

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../../store/CartContext/CartContext';
 
 const HeaderCartButton = ({ onClick }) => {
+  const context = useContext(CartContext);
+  const { items } = context;
+  const numberOfCartItems = items.reduce(
+    (total, item) => total + item.amount,
+    0
+  );
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
