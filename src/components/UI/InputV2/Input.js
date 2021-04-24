98038565870f992f60ad8fb0a2d7f365d23c30fe
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-24 23:16:57 +0300

    ref forward added to the input component (V2)

--- src/components/UI/InputV2/Input.js
+++ src/components/UI/InputV2/Input.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classes from './Input.module.css';
 
-const Input = ({ label, input }) => {
+const Input = ({ label, input }, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={input.id}>{label}</label>
-      <input {...input} />
+      <input {...input} ref={ref} />
     </div>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
