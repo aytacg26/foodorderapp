23a6f5d37d44b3b67fe8f880c0dd5ffbb992a1fa
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-24 23:17:53 +0300

    Cart Provider added to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartContext/CartProvider';
 
 const App = () => {
   const [showCart, setShowCart] = useState(false);
@@ -16,13 +17,13 @@ const App = () => {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {showCart && <Cart onClose={handleCart} onOrder={handleOrder} />}
       <Header onClick={handleCart} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 };
 
