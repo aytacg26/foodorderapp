53962ac1c8a513a78538248b31088f982fc67182
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-24 23:13:49 +0300

    Validation added to MealItemForm component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/InputV2/Input';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const inputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = parseInt(inputRef.current.value);
+
+    if (!enteredAmount || enteredAmount < 0 || enteredAmount > 10) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    setAmountIsValid(true);
+    props.onAddToCart(enteredAmount);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={inputRef}
         label='Amount'
         input={{
           id: 'amount',
           type: 'number',
           min: '1',
-          max: '5',
+
           step: '1',
           defaultValue: '1',
         }}
       />
       <button> + Add</button>
+      {!amountIsValid && (
+        <p style={{ color: 'red' }}>Please enter a valid amount (1-10)</p>
+      )}
     </form>
   );
 };
